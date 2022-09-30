3c4eb96aee0c41a26edb808103b3c0a9018135f7
Daearon <alexandr8769@mail.ru>
2022-09-30 07:57:35 +0300

    add ifelse

---
+++ Fifth_IF/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Рады тебе, Маша");
+}
+else 
+{
+    Console.Write("Здравствуй, ");
+    Console.Write(username);
+}
